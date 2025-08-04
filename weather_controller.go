c0d06ad9663e013095272c737389e9436d4301e4
Karn Wong <karn@karnwong.me>
2025-08-04 13:16:15 +0700

    feat: add support for latitude/longitude

--- weather_controller.go
+++ weather_controller.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/rs/zerolog/log"
 
 	owm "github.com/briandowns/openweathermap"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
+)
+
+var (
+	CityName  string
+	Latitude  float64
+	Longitude float64
 )
 
 type WeatherResponse struct {
@@ -21,10 +30,21 @@ type WeatherResponse struct {
 func WeatherGetController(c *fiber.Ctx) error {
 	w, err := owm.NewCurrent("C", "en", os.Getenv("OPENWEATHER_API_KEY"))
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal().Err(err).Msg("Failed to init OpenWeatherMap")
 	}
 
-	err = w.CurrentByName(os.Getenv("CURRENT_CITY"))
+	// prioritize lat/lng over city name
+	if Latitude != 0 {
+		log.Info().Msg("Using latitude/longitude for location")
+		err = w.CurrentByCoordinates(&owm.Coordinates{
+			Longitude: Longitude,
+			Latitude:  Latitude,
+		})
+	} else {
+		log.Info().Msg("Using city name for location")
+		err = w.CurrentByName(CityName)
+	}
+
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error": "Cannot obtain weather data",
@@ -40,4 +60,29 @@ func WeatherGetController(c *fiber.Ctx) error {
 		RainOneHour:   w.Rain.OneH,
 		RainThreeHour: w.Rain.ThreeH,
 	})
+}
+
+func stringToFloat(s string) (float64, error) {
+	vInt, err := strconv.ParseFloat(strings.TrimSpace(s), 64)
+	if err != nil {
+		return 0, err
+	} else {
+		return vInt, nil
+	}
+}
+
+func init() {
+	var err error
+
+	CityName = os.Getenv("CURRENT_CITY")
+	Latitude, err = stringToFloat(os.Getenv("LATITUDE"))
+	if err != nil {
+		log.Warn().Msg("Error converting latitude to float")
+		Latitude = 0
+	}
+	Longitude, err = stringToFloat(os.Getenv("LONGITUDE"))
+	if err != nil {
+		log.Warn().Msg("Error converting longitude to float")
+		Longitude = 0
+	}
 }
